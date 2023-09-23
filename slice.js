d1a76527e8d095eb4eae6112a43f3710a4d1b32c
Shajib96 <shajibahamedornob@gmail.com>
2023-09-24 01:13:14 +0600

    first commit

---
+++ slice.js
@@ -0,0 +1,9 @@
+// var numbers=[1,34,3,43,56,43,62,44,11,51,56,]
+// var number =numbers.slice(3 ,7)
+// console.log(number)
+
+var names='My name is Shajib sarkar';
+var name= names.includes("my")
+
+console.log(name)
+console.log(names)
