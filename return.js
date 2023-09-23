0f0077dd1cf7408f3cc9c862f207b8917521a3b6
Shajib96 <shajibahamedornob@gmail.com>
2023-09-24 03:13:20 +0600

    Code how work return methor

---
+++ return.js
@@ -0,0 +1,21 @@
+// function addition(number1, number2){
+//    console.log(number1,number2);
+//    var sum = number1+number2;
+//    return sum;
+// }
+// var total =addition(80,40)
+// console.log('Total',total)
+
+
+function bringWater(momGiven){
+    var perLiterWater = 0.70;
+    var totalWater = momGiven/0.70;
+    return totalWater
+}
+var MyTk = 5;
+var litter = bringWater(MyTk);
+var fixed = litter.toFixed(2);
+var number = parseFloat(fixed)
+console.log(number)
+console.log(fixed);
+console.log(litter);
