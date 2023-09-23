e770357bb648c437ddadd81201bd5dc5754c9867
Shajib96 <shajibahamedornob@gmail.com>
2023-09-24 03:44:48 +0600

    addition with fintion

---
+++ additiom.js
@@ -0,0 +1,9 @@
+function addition(number1 , number2){
+      const sum = number1+number2;
+      return sum;
+
+}
+const result1=addition(32,55);
+const result2 = addition(12,33);
+const finnalResult= addition(result1,result2);
+console.log(finnalResult);
