cdc4fd9f62ddb82235e242798ac7bf4f13bf57f7
Shajib96 <shajibahamedornob@gmail.com>
2023-09-24 02:38:28 +0600

    Add multiple paramiter in funtion

---
+++ multiple-function.js
@@ -0,0 +1,15 @@
+// function sum(a,b,c,d,e,f){
+//     var sum = a+b+c+d+e+f;
+//     console.log(sum);
+// }
+// sum(23,4,53,53,53,2,)
+
+function avaregs(math ,eng , bang,scie, bio,chem ,totalSub){
+      var avareg = math+eng+bang+scie+bio+chem/totalSub;
+      console.log(avareg);
+      console.log(avaregs)
+      var fixeds =avareg.toFixed(3)
+      console.log(fixeds)
+   
+}
+avaregs( 23,56,75,43,24,35,6)
---
+++ paramiter.js
@@ -0,0 +1,13 @@
+// function bringLove(money){
+//     console.log('If you have money you can buy love & happiness')
+//     console.log(money)
+// }
+// bringLove(30)
+
+function givenMoney(money){
+    console.log(money ,"Tk dilam mal den")
+    console.log('Mama ay nen tk singaara den')
+}
+
+var tk = 400;
+givenMoney(tk)
