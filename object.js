65850e95b14a1260229a2c45a9b5a1b9f18e420a
Shajib96 <shajibahamedornob@gmail.com>
2023-09-24 12:39:26 +0600

    Declare multiple objects with multiple properties

---
+++ object.js
@@ -0,0 +1,19 @@
+var mobile ={
+    brand:'Samsung',
+    price: 19000,
+    color :'Black',
+    storage:"64gb"
+
+}
+var myComputer = {
+    brand :"Samsung",
+    price:70000,
+    processor:'i7',
+    color:"Black"
+
+}
+console.log(mobile.brand);
+console.log(myComputer.color);
+console.log(myComputer.price);
+myComputer.price=4500;
+console.log(myComputer)
