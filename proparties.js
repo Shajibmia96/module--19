5a0811354a05cd951d28e73f788711366ea5bd29
Shajib96 <shajibahamedornob@gmail.com>
2023-09-24 13:09:03 +0600

    Object count & object name

---
+++ proparties.js
@@ -0,0 +1,15 @@
+var shoppingCard ={
+    shart:8,
+    pent :3,
+    t_shart:6,
+    Football:8,
+}
+// shoppingCard.Football=11;
+// var FootballCount=shoppingCard.Football
+
+// console.log(FootballCount)
+// console.log(shoppingCard)
+var pentCount =shoppingCard.pent
+console.log(pentCount)
+var shartCount =shoppingCard['shart']
+console.log(shartCount)
